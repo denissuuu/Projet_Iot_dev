b87b13490e6fb9292956161de791f4b9b4399d11
Denis <denis.onet@ynov.com>
2025-11-20 15:40:47 +0100

    added intru

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+
+
+// WiFi Manager
+// Reset WiFi Params
+// Appairage
+// MQTT
+// DHT / OLED / MOVEMENT
+
+void setup() {
+  #if defined(DEBUG)
+  Serial.begin(DEBUG_SPEED);
+  #endif
+}
+
+void loop() {
+
+  #if DELAY_COUCOU == 1
+  delay(5000); // Publish every 5 seconds
+  #elif DELAY_COUCOU == 2
+  delay(10000); // Publish every 10 seconds
+  #endif
+  #if defined (DEBUG)
+  Serial.println("coucou");
+  #endif
+}
